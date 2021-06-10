a79274658ac0d9060a5c121878417c8e1e8c2d7a
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2021-06-10 01:23:11 -0300

    Initial Commit

---
+++ BMOAulasCpp23ConstInCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BMOAulasCpp23ConstInCpp
+//
+//  Created by Breno Medeiros on 10/06/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
